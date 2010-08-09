bfd8ca6a27fe5aead90e98822056777feb735c30
lilyco <lilyco@ea7d387c-8a93-4eb2-b106-f7338a26bd29>
2010-08-09 13:36:04 +0000

    updated sanity test

--- sector-sphere/trunk/tests/sanity.sh
+++ sector-sphere/trunk/tests/sanity.sh
@@ -6,14 +6,14 @@ SECTOR=$(cd $(dirname $0)/..; echo $PWD)
 export PATH=$PATH:/sbin
 
 . $SECTOR/tests/test-framework.sh
-
+init_test_env $@
 
 #local test config
-SLAVE_COUNT=${SLAVE_COUNT:-"1"}
+SLAVE_COUNT=${SLAVE_COUNT:-"3"}
 
-SLAVE1_IP=${SLAVE1_IP:-"131.193.181.153"}
-SLAVE2_IP=${SLAVE2_IP:-"131.193.181.153"}
-SLAVE3_IP=${SLAVE3_IP:-"131.193.181.153"}
+SLAVE1_IP=${SLAVE1_IP:-"$SECTOR_HOST"}
+SLAVE2_IP=${SLAVE2_IP:-"$SECTOR_HOST"}
+SLAVE3_IP=${SLAVE3_IP:-"$SECTOR_HOST"}
 
 SLAVE1_DIR=${SLAVE1_DIR:-"$TMP/slave1"}
 SLAVE2_DIR=${SLAVE2_DIR:-"$TMP/slave2"}
@@ -30,8 +30,13 @@ setup() {
                 local SLAVE_NODE=SLAVE${i}_IP
                 mkdir -p SLAVE${i}_DIR
                 echo "start slave $i ...\n"
-                $DSH ${!SLAVE_NODE} "${START_SLAVE} $SECTOR & > /dev/null &" &
+                $DSH ${!SLAVE_NODE} "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SECTOR}/lib ${START_SLAVE} $SECTOR & > /dev/null &" &
          done
+
+#         REAL_SLAVES=`${SYSINFO} | grep Slave | awk '{print $6}'`
+#         if [ ${REAL_SLAVES} -ne ${SLAVE_COUNT} ]; then
+#            error "slave only setup ${REAL_SLAVES}, but require ${SLAVE_COUNT}"
+#         fi
 }
 
 cleanup() {
@@ -44,19 +49,75 @@ cleanup() {
          killall -9 start_master
 }
 
+build_test_filter
+
+cleanup
+
+if [ "$ONLY" == "cleanup" ]; then
+        cleanup
+        exit 0
+fi
+
 setup
 
+if [ "$ONLY" == "setup" ]; then
+        exit 0
+fi
+
 test_0() {
-        echo test0 > touch $TMP/test_0
-        $UPLOAD $TMP/test_0 /
-        $LS / 
-        $LS / | grep test_0 || error "test1 failed: did not find test_0"
-        $RM /test_0 
-        $LS / 
-        $LS / | grep test_0 && error "test1 failed: did not rm test_0"
-        rm -rf $TMP/test_0
+        echo test0 > $TMP/$tfile
+        $RM /$tfile 
+        $UPLOAD $TMP/$tfile /
+        $LS / | grep $tfile || error "failed: did not find $tfile"
+        $RM /$tfile 
+        $LS / | grep $tfile && error "failed: did not rm $tfile"
+        rm -rf $TMP/$tfile
 }
 run_test 0 "upload .../file ; ls..../file rm .../file =============="
+
+test_1() {
+        $MKDIR $tdir
+        $LS / | grep $tdir || error "failed: did not find $tdir"
+        $RM /$tdir 
+        $LS / | grep $tdir && error "failed: did not rm $tdir"
+}
+run_test 1 "mkdir .../dir ; ls..../dir rm .../dir =============="
+
+test_2() {
+        echo test0 > $TMP/$tfile
+        $UPLOAD $TMP/$tfile /
+        $MKDIR $tdir
+        $MV /$tfile /${tfile}_1 || error "failed: mv failed "
+        $LS / | grep ${tfile}_1 || error "failed: did not mv ${tfile} to ${tfile}_1"
+        rm -rf $TMP/$tfile
+}
+run_test 2 "upload .../file ; mv..file /dir rm .../dir =============="
+
+test_3() {
+        echo test0 > $TMP/$tfile
+        $RM /$tfile
+        $RM /${tfile}_1
+        $UPLOAD $TMP/$tfile /
+        $MV /$tfile /${tfile}_1
+        $DOWNLOAD /${tfile}_1 $TMP/
+        diff $TMP/$tfile $TMP/${tfile}_1 || error "download got different file!"
+        rm -rf $TMP/$tfile
+        rm -rf $TMP/${tfile}_1
+}
+run_test 3 "upload .../file ; download .../file compare =============="
+
+test_4() {
+        echo test0 > $TMP/$tfile
+        $RM /$tfile
+        $UPLOAD $TMP/$tfile /
+        $MKDIR /$tdir
+        $COPY /$tfile /${tdir}
+        $LS /$tdir | grep $tfile && error "failed: did not copy $tdir"
+        $DOWNLOAD /$tdir/$tfile $TMP/${tfile}_download
+        diff $TMP/$tfile $TMP/${tfile}_download || error "download got different file!"
+        rm -rf $TMP/${tfile}_download
+}
+run_test 4 "upload ...tfile, copy ...tfile /tdir, download tdir/tfile tfile_download, diff tfile_download tfile..."
 
 cleanup
 
--- sector-sphere/trunk/tests/test-framework.sh
+++ sector-sphere/trunk/tests/test-framework.sh
@@ -1,35 +1,124 @@
 #!/bin/bash
 # vim:expandtab:shiftwidth=4:softtabstop=4:tabstop=4:
 
-export START_MASTER=${START_MASTER:-"${SECTOR}/master/start_master"}
-export START_SECURITY=${START_SECURITY:-"${SECTOR}/security/sserver"}
-export START_SLAVE=${START_SLAVE:-"${SECTOR}/slave/start_slave"}
-export MASTER_CONF=${MASTER_CONF:-"${SECTOR}/conf/master.conf"}
-export SAVE_PWD=${SAVE_PWD:-${SECTOR}/tests}
-export COPY=${COPY:-${SECTOR}/tools/sector_cp}
-export MKDIR=${MKDIR:-${SECTOR}/tools/sector_mkdir}
-export LS=${LS:-${SECTOR}/tools/sector_ls}
-export MV=${MV:-${SECTOR}/tools/sector_mv}
-export RM=${RM:-${SECTOR}/tools/sector_rm}
-export UPLOAD=${UPLOAD:-${SECTOR}/tools/sector_upload}
-export DSH=${DSH:-"ssh"}
+absolute_path() {
+    (cd `dirname $1`; echo $PWD/`basename $1`)
+}
+
+init_test_env() {
+        export SECTOR=`absolute_path $SECTOR`
+        export SECTOR_HOME=`absolute_path $SECTOR`
+        export TESTSUITE=`basename $0 .sh`
+        export START_MASTER=${START_MASTER:-"${SECTOR}/master/start_master"}
+        export START_SECURITY=${START_SECURITY:-"${SECTOR}/security/sserver"}
+        export START_SLAVE=${START_SLAVE:-"${SECTOR}/slave/start_slave"}
+        export MASTER_CONF=${MASTER_CONF:-"${SECTOR}/conf/master.conf"}
+        export SAVE_PWD=${SAVE_PWD:-${SECTOR}/tests}
+        export COPY=${COPY:-${SECTOR}/tools/sector_cp}
+        export MKDIR=${MKDIR:-${SECTOR}/tools/sector_mkdir}
+        export LS=${LS:-${SECTOR}/tools/sector_ls}
+        export MV=${MV:-${SECTOR}/tools/sector_mv}
+        export RM=${RM:-${SECTOR}/tools/sector_rm}
+        export UPLOAD=${UPLOAD:-${SECTOR}/tools/sector_upload}
+        export DOWNLOAD=${DOWNLOAD:-${SECTOR}/tools/sector_download}
+        export SYSINFO=${SYSINFO:-${SECTOR}/tools/sector_sysinfo}
+        export DSH=${DSH:-"ssh"}
+        export SECTOR_HOST=${SECTOR_HOST:-`hostname`}
+        export TEST_FAILED=false
+
+        if ! echo $PATH | grep -q $SECTOR/tests; then
+            export PATH=$PATH:$SECTOR/tests
+        fi
+
+        if ! echo $PATH | grep -q $SECTOR/lib; then
+            export PATH=$PATH:$SECTOR/lib
+        fi
+
+        if ! echo $LD_LIBRARY_PATH | grep -q $SECTOR/lib; then
+            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SECTOR/lib
+        fi
+
+        ONLY=${ONLY:-$*}
+        echo $PATH
+}
+
+build_test_filter() {
+    [ "$ONLY" ] && echo "only running test `echo $ONLY`"
+    for O in $ONLY; do
+        eval ONLY_${O}=true 
+    done
+}
+
+error_no_exit() {
+        echo $@
+        TEST_FAILED=true
+}
 
 error() {
-        echo $@
-        exit 1 
+        error_no_exit
+        exit 1
+}
+
+basetest() {
+    if [[ $1 = [a-z]* ]]; then
+        echo $1
+    else
+        echo ${1%%[a-z]*}
+    fi
+}
+
+pass() {
+    $TEST_FAILED && echo -n "FAIL " || echo -n "PASS "
+    echo $@
+}
+
+run_one() {
+        local testnum=$1
+        local message=$2
+        tfile=f${testnum}
+        export tdir=d${base}
+        export TESTNAME=test_$testnum
+
+        local BEFORE=`date +%s`
+        echo "== test $testnum: $message == `date +%H:%M:%S` ($BEFORE)"
+        TEST_FAILED=false
+        test_${testnum} || echo "test_$testnum failed with $?"
+
+        pass "($((`date +%s` - $BEFORE))s)"
+        TEST_FAILED=false
+        cd $SAVED_PWD 
+        unset TESTNAME
+        unset tdir
+        unset tfile
+        unset base
+        return $?
 }
 
 run_test() {
-    local BEFORE=`date +%s`
-    local testnum=$1
-    local message=$2
-    export TESTNAME=test_$testnum
+    export base=`basetest $1`
+    if [ ! -z "$ONLY" ]; then
+        testname=ONLY_$1
+        if [ ${!testname}x != x ]; then
+            [ "$LAST_SKIPPED" ] && echo "" && LAST_SKIPPED=
+            run_one $1 "$2"
+            return $?
+        fi
+        testname=ONLY_$base
+        if [ ${!testname}x != x ]; then
+            [ "$LAST_SKIPPED" ] && echo "" && LAST_SKIPPED=
+            run_one $1 "$2"
+            return $?
+        fi
+        LAST_SKIPPED="y"
+        echo -n "."
+        return 0
+    fi
 
-    test_${testnum} || echo "test_$testnum failed with $?"
+    LAST_SKIPPED=
+    run_one $1 "$2"
 
-    cd $SAVED_PWD 
-    unset TESTNAME
-    unset tdir
     return $?
 }
+
+
 
