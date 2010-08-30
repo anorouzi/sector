ca6317dead5815be67fcd5af32d5db6238e9dd8c
lilyco <lilyco@ea7d387c-8a93-4eb2-b106-f7338a26bd29>
2010-08-30 16:11:17 +0000

    forgot firs "/" in the last revision

--- sector-sphere/trunk/fuse/sectorfs.h
+++ sector-sphere/trunk/fuse/sectorfs.h
@@ -1,4 +1,4 @@
-*****************************************************************************
+/*****************************************************************************
 Copyright 2005 - 2010 The Board of Trustees of the University of Illinois.
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not
